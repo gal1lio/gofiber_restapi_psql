288a42d985167dc1265840a36ad7d434ed05a03c
gal1lio <axlj4y@gmail.com>
2020-10-14 12:33:47 +0800

    Initial commit with functionality

---
+++ database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE fiber_demo;
+
+CREATE TABLE employee(
+    demo_id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    salary VARCHAR(255),
+    age VARCHAR(255)
+);
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/gal1lio/projects/fiber_psql
+
+go 1.15
+
+require (
+	github.com/gofiber/fiber/v2 v2.0.6
+	github.com/lib/pq v1.8.0
+)
